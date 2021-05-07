25b98bc359f05fa7ba3c1088b6d82467816b1589
Seo Myung Ryul <smr3102@naver.com>
2021-05-07 17:33:30 +0900

    [FEAT] MainViewController 파일 생성
    - MainViewController 파일 생성
    - 모든 VC가 채택할 ViewModelBindableType Protocol 생성

---
+++ ZURAZU/ZURAZU/Common/ViewModelBindableType.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelBindableType.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/07.
+//
+
+import Foundation
---
+++ ZURAZU/ZURAZU/Main/View/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/07.
+//
+
+import UIKit
+import Combine
+
+final class MainViewController: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    
+  }
+}
+
+private extension MainViewController {
+  
+  func setupConstraint() {
+    
+  }
+}
