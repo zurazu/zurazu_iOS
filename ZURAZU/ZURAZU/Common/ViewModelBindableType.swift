4cd23261d151d7e85494740db0bc076a878b7731
Seo Myung Ryul <smr3102@naver.com>
2021-05-07 17:34:55 +0900

    [FEAT] ViewModelBindableType 파일 작성
    - 코디네이터에서 VC를 생성한 후 ViewModel을 주입할 수 있도록 코드 작성
    - 코디네이터에서 viewController.bind(viewModel:)을 사용
    - protocol을 사용하므로 viewModel변수에 접근할 수 있는 위험 존재

--- ZURAZU/ZURAZU/Common/ViewModelBindableType.swift
+++ ZURAZU/ZURAZU/Common/ViewModelBindableType.swift
@@ -5,4 +5,23 @@
 //  Created by 서명렬 on 2021/05/07.
 //
 
-import Foundation
+import UIKit
+
+protocol ViewModelBindableType {
+  
+  associatedtype ViewModelType
+  
+  var viewModel: ViewModelType! { get set }
+  
+  func bindViewModel()
+}
+
+extension ViewModelBindableType where Self: UIViewController {
+  
+  mutating func bind(viewModel: ViewModelType) {
+    self.viewModel = viewModel
+    self.loadViewIfNeeded()
+    
+    self.bindViewModel()
+  }
+}
