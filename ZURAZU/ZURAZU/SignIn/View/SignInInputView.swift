201a9012272832bf5861cf0b154c4a56b9c9c0e4
Seo Myung Ryul <smr3102@naver.com>
2021-05-11 20:24:08 +0900

    [FEAT] SignInInputView 작성
    - emailInputView와 passwordInputView를 묶어서 하나의 컴포넌트로 활용
    - 로그인 label 추가

---
+++ ZURAZU/ZURAZU/SignIn/View/SignInInputView.swift
@@ -0,0 +1,59 @@
+//
+//  SignInInputView.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/11.
+//
+
+import UIKit
+
+final class SignInInputView: UIView {
+  
+  private let emailInputView: InputView = .init(frame: .zero, inputViewType: .email)
+  private let passwordInputView: InputView = .init(frame: .zero, inputViewType: .password)
+  private let signInLable: UILabel = .init(frame: .zero)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupView()
+    setupConstraint()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+private extension SignInInputView {
+  
+  func setupConstraint() {
+    [signInLable, emailInputView, passwordInputView].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      addSubview($0)
+    }
+    
+    NSLayoutConstraint.activate([
+      signInLable.topAnchor.constraint(equalTo: topAnchor),
+      signInLable.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+      signInLable.heightAnchor.constraint(equalToConstant: 15.6),
+      signInLable.widthAnchor.constraint(greaterThanOrEqualToConstant: 0),
+      
+      emailInputView.topAnchor.constraint(equalTo: signInLable.bottomAnchor, constant: 28),
+      emailInputView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 22),
+      emailInputView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -22),
+      emailInputView.heightAnchor.constraint(equalToConstant: 40),
+      
+      passwordInputView.topAnchor.constraint(equalTo: emailInputView.bottomAnchor, constant: 25),
+      passwordInputView.leadingAnchor.constraint(equalTo: emailInputView.leadingAnchor),
+      passwordInputView.trailingAnchor.constraint(equalTo: emailInputView.trailingAnchor),
+      passwordInputView.heightAnchor.constraint(equalTo: emailInputView.heightAnchor)
+      
+    ])
+  }
+  
+  func setupView() {
+    signInLable.text = "로그인"
+    signInLable.font = UIFont.systemFont(ofSize: 13)
+  }
+}
