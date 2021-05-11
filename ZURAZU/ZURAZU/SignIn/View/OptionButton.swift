fcea3923f4c62d33f30aed076ef0a29dfd05c56e
Seo Myung Ryul <smr3102@naver.com>
2021-05-11 21:30:29 +0900

    [FEAT] OptionStackView 작성
    - 회원가입, 이메일 찾기, 비밀번호 찾기 버튼을 포함한 StackView 작성
    - 각 버튼은 OptionButton을 통해 생성

---
+++ ZURAZU/ZURAZU/SignIn/View/OptionButton.swift
@@ -0,0 +1,26 @@
+//
+//  OptionButton.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/11.
+//
+
+import UIKit
+
+final class OptionButton: UIButton {
+  
+  convenience init(title: String) {
+    self.init(frame: .zero)
+    
+    setTitle(title, for: .normal)
+    setupView()
+  }
+}
+
+private extension OptionButton {
+  func setupView() {
+    backgroundColor = .white
+    setTitleColor(.black, for: .normal)
+    titleLabel?.font = UIFont.systemFont(ofSize: 11)
+  }
+}
