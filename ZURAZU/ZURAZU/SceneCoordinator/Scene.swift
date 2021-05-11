e5d8908d06a21ad59ce453225da38e0f15774afa
Seo Myung Ryul <smr3102@naver.com>
2021-05-11 15:33:55 +0900

    [FEAT] Scene 프로토콜 확장
    - NavC나 VC 를 생성할 때 중복되는 코드를 extension으로 확장

--- ZURAZU/ZURAZU/SceneCoordinator/Scene.swift
+++ ZURAZU/ZURAZU/SceneCoordinator/Scene.swift
@@ -13,3 +13,21 @@ protocol Scene {
   
   func instantiate() -> UIViewController
 }
+
+extension Scene {
+  
+  func navigationController(identifier: String) -> UINavigationController? {
+    let storyboard = UIStoryboard(name: self.storyboard, bundle: nil)
+    
+    let navigationController = storyboard.instantiateViewController(identifier: identifier) as? UINavigationController
+    
+    return navigationController
+  }
+  
+  func viewController(identifier: String) -> UIViewController {
+    let storyboard = UIStoryboard(name: self.storyboard, bundle: nil)
+    let viewController = storyboard.instantiateViewController(identifier: identifier)
+    
+    return viewController
+  }
+}
