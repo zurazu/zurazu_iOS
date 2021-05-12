59a88a7620b45937cfb8a82fc624044d6b4af195
itzel.du <itzel.du@kakaoenterprise.com>
2021-05-12 14:47:08 +0900

    [FEAT] Color 및 Font Constants 생성
    - Color Assets 추가
    - 재사용을 위해 UIColor 및 UIFont에 상수 추가
    - NSAttributedString에서 사용할 Attributes 객체 추가

---
+++ ZURAZU/ZURAZU/Util/UIFont+Named.swift
@@ -0,0 +1,35 @@
+//
+//  UIFont+Named.swift
+//  ZURAZU
+//
+//  Created by itzel.du on 2021/05/12.
+//
+
+import UIKit
+
+enum FontSize {
+    
+    static let primary     : CGFloat = 17
+    static let secondary   : CGFloat = 15
+    static let tertiary    : CGFloat = 13
+    static let quaternary  : CGFloat = 11
+    static let smallest    : CGFloat = 10
+}
+
+extension UIFont {
+    
+    static let primaryBold    : UIFont = UIFont.systemFont(ofSize: FontSize.primary, weight: .bold)
+    static let primary        : UIFont = UIFont.systemFont(ofSize: FontSize.primary)
+    
+    static let secondaryBold  : UIFont = UIFont.systemFont(ofSize: FontSize.secondary, weight: .bold)
+    static let secondary      : UIFont = UIFont.systemFont(ofSize: FontSize.secondary)
+    
+    static let tertiaryBold   : UIFont = UIFont.systemFont(ofSize: FontSize.tertiary, weight: .bold)
+    static let tertiary       : UIFont = UIFont.systemFont(ofSize: FontSize.tertiary)
+    
+    static let quaternaryBold : UIFont = UIFont.systemFont(ofSize: FontSize.quaternary, weight: .bold)
+    static let quaternary     : UIFont = UIFont.systemFont(ofSize: FontSize.quaternary)
+    
+    static let smallestBold   : UIFont = UIFont.systemFont(ofSize: FontSize.smallest, weight: .bold)
+    static let smallest       : UIFont = UIFont.systemFont(ofSize: FontSize.smallest)
+}
