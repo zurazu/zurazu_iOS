52c05531415fb8a3625d99f51902055bcf28dc79
Seo Myung Ryul <smr3102@naver.com>
2021-05-13 16:46:20 +0900

    [FEAT] Constant 파일 컨벤션 적용
    - indent 2space로 적용
    - Constant 컨벤션 적용

--- ZURAZU/ZURAZU/Util/NSAttributedString+CustomAttribute.swift
+++ ZURAZU/ZURAZU/Util/NSAttributedString+CustomAttribute.swift
@@ -8,49 +8,49 @@
 import UIKit
 
 enum Attributes {
-    
-    typealias AttributesOfNSAttributedString = [NSAttributedString.Key: Any]
-    
-    static let category: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoPrimary,
-        .font            : UIFont.primary,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
-    static let categoryBold: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoQuaternary,
-        .font            : UIFont.primary,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
-    static let brandPrimary: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoTertiary,
-        .font            : UIFont.smallest,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
-    static let wearPrimary: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoPrimary,
-        .font            : UIFont.tertiary,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
-    static let pricePrimary: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoPrimary,
-        .font            : UIFont.secondary,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
-    static let wearSecondary: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoPrimary,
-        .font            : UIFont.quaternary,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
-    static let priceSecondary: AttributesOfNSAttributedString = [
-        .foregroundColor : UIColor.monoPrimary,
-        .font            : UIFont.tertiary,
-        .paragraphStyle  : NSTextAlignment.left
-    ]
-    
+  
+  typealias AttributesOfNSAttributedString = [NSAttributedString.Key: Any]
+  
+  static let category: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoPrimary,
+    .font            : UIFont.primary,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
+  static let categoryBold: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoQuaternary,
+    .font            : UIFont.primary,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
+  static let brandPrimary: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoTertiary,
+    .font            : UIFont.smallest,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
+  static let wearPrimary: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoPrimary,
+    .font            : UIFont.tertiary,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
+  static let pricePrimary: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoPrimary,
+    .font            : UIFont.secondary,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
+  static let wearSecondary: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoPrimary,
+    .font            : UIFont.quaternary,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
+  static let priceSecondary: AttributesOfNSAttributedString = [
+    .foregroundColor : UIColor.monoPrimary,
+    .font            : UIFont.tertiary,
+    .paragraphStyle  : NSTextAlignment.left
+  ]
+  
 }
--- ZURAZU/ZURAZU/Util/UIImage+Constant.swift
+++ ZURAZU/ZURAZU/Util/UIImage+Constant.swift
@@ -12,11 +12,11 @@ extension UIImage {
   static let errorImage = #imageLiteral(resourceName: "errorImage")
   
   static let textAlignLeft: UIImage = UIImage(systemName: "text.alignleft") ?? errorImage
-  static let docText:       UIImage = UIImage(systemName: "doc.text")       ?? errorImage
-  static let heart:         UIImage = UIImage(systemName: "heart")          ?? errorImage
-  static let person:        UIImage = UIImage(systemName: "person")         ?? errorImage
+  static let docText      : UIImage = UIImage(systemName: "doc.text")       ?? errorImage
+  static let heart        : UIImage = UIImage(systemName: "heart")          ?? errorImage
+  static let person       : UIImage = UIImage(systemName: "person")         ?? errorImage
   
-  static let docTextFill:   UIImage = UIImage(systemName: "doc.text.fill")  ?? errorImage
-  static let heartFill:     UIImage = UIImage(systemName: "heart.fill")     ?? errorImage
-  static let personFill:    UIImage = UIImage(systemName: "person.fill")    ?? errorImage
+  static let docTextFill  : UIImage = UIImage(systemName: "doc.text.fill")  ?? errorImage
+  static let heartFill    : UIImage = UIImage(systemName: "heart.fill")     ?? errorImage
+  static let personFill   : UIImage = UIImage(systemName: "person.fill")    ?? errorImage
 }
