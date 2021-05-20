c38537eb126c4a207fdb374e72ee7cfc92d50f66
Seo Myung Ryul <smr3102@naver.com>
2021-05-20 17:37:11 +0900

    [FEAT] CategoryDetailVC 초기 설정
    - CategoryDetailScene 작성
    - CategoryDetailViewController 및 storyboard 생성

---
+++ ZURAZU/ZURAZU/CategoryDetail/View/CategoryDetailScene.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryDetailScene.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/20.
+//
+
+import UIKit
+
+struct CategoryDetailScene: Scene {
+  
+  var storyboard: String {
+    return ""
+  }
+  
+  func instantiate() -> UIViewController {
+    guard var categoryDetailViewController: CategoryDetailViewController = self.viewController(identifier: .categoryDetailVC) as? CategoryDetailViewController else { fatalError() }
+    
+    let viewModel: CategoryDetailViewModel = .init()
+    
+    categoryDetailViewController.bind(viewModel: viewModel)
+    
+    return categoryDetailViewController
+  }
+}
---
+++ ZURAZU/ZURAZU/CategoryDetail/View/CategoryDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryDetailViewController.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/20.
+//
+
+import UIKit
+
+final class CategoryDetailViewController: UIViewController, ViewModelBindableType {
+  
+  var viewModel: CategoryDetailViewModelType?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  func bindViewModel() {
+    
+  }
+}
