14cf2306d707a5e7eb834a32bab9c6979449c508
Seo Myung Ryul <smr3102@naver.com>
2021-05-27 18:13:39 +0900

    [FEAT] MainCategory 모델 작성
    - CategoryVC에 나타낼 category의 모델 작성

---
+++ ZURAZU/ZURAZU/Category/Model/MainCategory.swift
@@ -0,0 +1,16 @@
+//
+//  MainCategory.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/27.
+//
+
+import Foundation
+
+struct MainCategory: Decodable {
+  
+  let idx: Int
+  let korean: String
+  let english: String
+  let priority: Int
+}
