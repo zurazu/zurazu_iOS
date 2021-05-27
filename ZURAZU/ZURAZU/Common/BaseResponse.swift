b20d5bbc938f68f8da65f6512c9d289c24cc9e43
Seo Myung Ryul <smr3102@naver.com>
2021-05-27 18:11:04 +0900

    [FEAT] BaseResponse 작성
    - 모든 response의 기본이 될 struct작성
    - list에 필요한 구조를 작성하여 생성할 수 있도록 구현

---
+++ ZURAZU/ZURAZU/Common/BaseResponse.swift
@@ -0,0 +1,17 @@
+//
+//  BaseResponse.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/05/27.
+//
+
+import Foundation
+
+struct BaseResponse<T: Decodable> {
+  
+  let id: String
+  let dateTime: String
+  let status: String
+  let message: String
+  let list: T?
+}
