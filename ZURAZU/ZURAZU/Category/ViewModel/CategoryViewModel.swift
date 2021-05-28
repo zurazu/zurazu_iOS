d4c8f0031420efe084fe24c499809ccf519c859b
Seo Myung Ryul <smr3102@naver.com>
2021-05-28 21:00:06 +0900

    [FEAT] CategoryViewModel 작성
    - API를 통해 MainCategories를 받는 로직 작성
    - MARK 주석 주의

--- ZURAZU/ZURAZU/Category/ViewModel/CategoryViewModel.swift
+++ ZURAZU/ZURAZU/Category/ViewModel/CategoryViewModel.swift
@@ -6,13 +6,56 @@
 //
 
 import Foundation
+import Combine
 
 protocol CategoryViewModelType {
   
-  var categoryTypes: [CategoryType] { get }
+  var mainCategories: PassthroughSubject<[MainCategory], Never> { get }
+  var fetchMainCategories: PassthroughSubject<Void, Never> { get }
 }
 
 final class CategoryViewModel: CategoryViewModelType {
   
-  var categoryTypes: [CategoryType] = CategoryType.allCases
+  private let sceneCoordinator: SceneCoordinatorType
+  private var cancellables: Set<AnyCancellable> = []
+  
+  var mainCategories: PassthroughSubject<[MainCategory], Never> = .init()
+  var fetchMainCategories: PassthroughSubject<Void, Never> = .init()
+  
+  init(sceneCoordinator: SceneCoordinatorType) {
+    self.sceneCoordinator = sceneCoordinator
+    
+    bind()
+  }
+}
+
+private extension CategoryViewModel {
+  
+  func bind() {
+    fetchMainCategories.sink { [weak self] in
+      self?.requestMainCategories()
+    }
+    .store(in: &cancellables)
+  }
+  
+  func requestMainCategories() {
+    // MARK: - Router를 어디서 주입할지 아니면 싱글톤으로 사용할지 논의해야합니다
+    let router = Router()
+    
+    let testPublisher: AnyPublisher<Result<BaseResponse<MainCategory>, NetworkError>, Never> = router.request(route: MainCategoryEndPoint.requestMainCategories)
+    
+    testPublisher.sink { result in
+      switch result {
+      case .success(let responseResult):
+        guard let mainCategories = responseResult.list else { return }
+        // MARK: - main큐에서 진행할지 커스텀 큐 사용할지 알아보고 수정할 것.
+        DispatchQueue.main.sync {
+          self.mainCategories.send(mainCategories)
+        }
+        
+      case .failure(let error):
+        print(error.localizedDescription)
+      }
+    }.store(in: &cancellables)
+  }
 }
