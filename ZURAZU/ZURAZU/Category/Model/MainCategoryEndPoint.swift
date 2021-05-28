8de4ff8d8174d99610ae1c03cf31dbeb0f266738
Seo Myung Ryul <smr3102@naver.com>
2021-05-28 18:20:37 +0900

    [FEAT] MainCategoryEndPoint 작성
    - MainCategory화면과 관련된 API 호출을 위한 EndPoint 작성

--- ZURAZU/ZURAZU/Category/Model/MainCategoryEndPoint.swift
+++ ZURAZU/ZURAZU/Category/Model/MainCategoryEndPoint.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+enum MainCategoryEndPoint {
+  
+  case requestMainCategories
+}
+
+extension MainCategoryEndPoint: EndPointable {
+  
+  var environmentBaseURL: String {
+    return APICredentials.ip.rawValue
+  }
+  
+  var baseURL: URLComponents {
+    switch self {
+    case .requestMainCategories:
+      guard let url = URLComponents(string: environmentBaseURL + APICredentials.mainCategories.rawValue)
+      else { fatalError() }
+      
+      return url
+    }
+  }
+  
+  var query: HTTPQuery? {
+    return nil
+  }
+  
+  var httpMethod: HTTPMethod? {
+    return .get
+  }
+  
+  var headers: HTTPHeader? {
+    return [
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    ]
+  }
+  
+  var bodies: HTTPBody? {
+    return nil
+  }
+}
