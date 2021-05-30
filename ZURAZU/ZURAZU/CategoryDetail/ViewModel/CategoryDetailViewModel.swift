b342a728f3e00a1a0fe221a3d5e7bb44cab3be8b
Seo Myung Ryul <smr3102@naver.com>
2021-05-30 20:06:35 +0900

    [FEAT] SubCategoryViewModel 작성
    - API를 통해 SubCategories를 fetch 받아올 수 있도록 구현
    - SceneCoordinator를 통해 화면 전환 코드 추가

--- ZURAZU/ZURAZU/CategoryDetail/ViewModel/CategoryDetailViewModel.swift
+++ ZURAZU/ZURAZU/CategoryDetail/ViewModel/CategoryDetailViewModel.swift
@@ -6,11 +6,68 @@
 //
 
 import Foundation
+import Combine
 
 protocol CategoryDetailViewModelType {
   
+  var subCategories: PassthroughSubject<[SubCategory], Never> { get }
+  var startFetching: PassthroughSubject<Void, Never> { get }
+  var close: PassthroughSubject<Void, Never> { get }
 }
 
 final class CategoryDetailViewModel: CategoryDetailViewModelType {
   
+  private let indexPath: IndexPath
+  private var cancellables: Set<AnyCancellable> = []
+  
+  var subCategories: PassthroughSubject<[SubCategory], Never> = .init()
+  var startFetching: PassthroughSubject<Void, Never> = .init()
+  var close: PassthroughSubject<Void, Never> = .init()
+  
+  init(indexPath: IndexPath) {
+    self.indexPath = indexPath
+    
+    bind()
+  }
+  
+  func bind() {
+    startFetching
+      .sink { [weak self] in
+        self?.fetchSubCategories()
+      }
+      .store(in: &cancellables)
+    
+    close
+      .sink { [weak self] in
+        self?.closeView()
+      }
+      .store(in: &cancellables)
+  }
+}
+
+private extension CategoryDetailViewModel {
+  
+  func fetchSubCategories() {
+    let network = NetworkProvider()
+    
+    let subCategoryPublisher: AnyPublisher<Result<BaseResponse<SubCategory>, NetworkError>, Never> = network.request(route: CategoryDetailEndPoint.subCategories(mainIndex: indexPath.row))
+    
+    subCategoryPublisher
+      .receive(on: Scheduler.mainScheduler)
+      .sink { [weak self] result in
+        switch result {
+        case .success(let responseResult):
+          guard let list = responseResult.list else { return }
+          
+          self?.subCategories.send(list)
+        case .failure(let error):
+          print(error.localizedDescription)
+        }
+      }
+      .store(in: &cancellables)
+  }
+  
+  func closeView() {
+    SceneCoordinator.shared.close(animated: true)
+  }
 }
