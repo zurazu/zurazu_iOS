07bc259cea08268f3504d34ac129d0ee5b280445
Seo Myung Ryul <smr3102@naver.com>
2021-06-01 20:22:49 +0900

    [FEAT] UICollectionView Register관련 확장코드 작성
    - register를 더 편하게 하기 위해 UICollectionView 확장

--- ZURAZU/ZURAZU/Util/UITableView+Register.swift
+++ ZURAZU/ZURAZU/Util/UITableView+Register.swift
@@ -10,24 +10,24 @@ import UIKit
 extension UITableView {
   
   func register<T: UITableViewCell>(_: T.Type) where T: Reusable {
-      register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+    register(T.self, forCellReuseIdentifier: T.defaultReuseIdentifier)
+  }
+  
+  func register<T: UITableViewCell>(_: T.Type) where T: Reusable, T: NibLoadable {
+    let bundle = Bundle(for: T.self)
+    let nib = UINib(nibName: T.nibName, bundle: bundle)
+    
+    register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
+  }
+  
+  func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: Reusable {
+    guard let cell: T = dequeueReusableCell(
+      withIdentifier: T.defaultReuseIdentifier,
+      for: indexPath
+    ) as? T else {
+      return T()
     }
     
-    func register<T: UITableViewCell>(_: T.Type) where T: Reusable, T: NibLoadable {
-      let bundle = Bundle(for: T.self)
-      let nib = UINib(nibName: T.nibName, bundle: bundle)
-      
-      register(nib, forCellReuseIdentifier: T.defaultReuseIdentifier)
-    }
-    
-    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: Reusable {
-      guard let cell: T = dequeueReusableCell(
-        withIdentifier: T.defaultReuseIdentifier,
-        for: indexPath
-      ) as? T else {
-        return T()
-      }
-      
-      return cell
-    }
+    return cell
+  }
 }
