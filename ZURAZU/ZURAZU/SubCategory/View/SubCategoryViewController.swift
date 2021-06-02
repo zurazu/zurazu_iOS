6ee5839feb6439b84018d45b824b94bd82790126
Seo Myung Ryul <smr3102@naver.com>
2021-06-02 17:40:06 +0900

    [FEAT] SubCategoryCollectionView 추가 및 바인딩
    - viewDidLayoutSubView() 시에 첫번째 셀이 선택되도록 구현
    - cell이 선택되는 tapPublisher 구현

--- ZURAZU/ZURAZU/SubCategory/View/SubCategoryViewController.swift
+++ ZURAZU/ZURAZU/SubCategory/View/SubCategoryViewController.swift
@@ -21,6 +21,7 @@ final class SubCategoryViewController: UIViewController, ViewModelBindableType {
     
     return button
   }()
+  private let categoryCollectionView: SubCategoryCollectionView = .init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
   
   private var cancellables: Set<AnyCancellable> = []
   
@@ -33,18 +34,42 @@ final class SubCategoryViewController: UIViewController, ViewModelBindableType {
     self.viewModel?.startFetching.send()
   }
   
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    self.viewModel?.selectedCategoryIndex.send(IndexPath.first)
+  }
+  
   func bindViewModel() {
     viewModel?.subCategories
       .receive(on: Scheduler.main)
-      .sink(receiveValue: { subCategories in
-        // MARK: - 수정 필요
-        print(subCategories)
-      })
+      .bind(
+        subscriber: categoryCollectionView.itemsSubscriber(
+          cellIdentifier: "SubCategoryCollectionViewCell",
+          cellType: SubCategoryCollectionViewCell.self,
+          cellConfig: { cell, _, subCategory in
+            cell.updateCell(withSubCategory: subCategory)
+          }
+        )
+      )
       .store(in: &cancellables)
-
+    
+    viewModel?.selectedCategoryIndex
+      .sink { [weak self] in
+        self?.categoryCollectionView.selectCell(at: $0)
+      }
+      .store(in: &cancellables)
+    
     backButton.tapPublisher
       .sink { [weak self] in
         self?.viewModel?.close.send()
+      }
+      .store(in: &cancellables)
+    
+    categoryCollectionView.didSelectItemPublisher
+      .receive(on: Scheduler.main)
+      .sink { [weak self] in
+        self?.viewModel?.selectedCategoryIndex.send($0)
       }
       .store(in: &cancellables)
   }
@@ -58,6 +83,16 @@ private extension SubCategoryViewController {
   }
   
   func setupConstraint() {
+    [categoryCollectionView].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview($0)
+    }
     
+    NSLayoutConstraint.activate([
+      categoryCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      categoryCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      categoryCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      categoryCollectionView.heightAnchor.constraint(equalToConstant: 44)
+    ])
   }
 }
