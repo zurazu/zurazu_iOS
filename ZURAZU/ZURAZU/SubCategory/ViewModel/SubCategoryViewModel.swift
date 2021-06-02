81ef05ff45558f03e82a1885755c764ac5d23f90
Seo Myung Ryul <smr3102@naver.com>
2021-06-02 20:52:59 +0900

    [FEAT] SubCategoryViewModel 수정
    - SubCategory가 선택될 때 일어나는 동작 바인딩
    - "전체" 카테고리 관련해서 하드코딩으로 넣어주는데 수정 필요

--- ZURAZU/ZURAZU/SubCategory/ViewModel/SubCategoryViewModel.swift
+++ ZURAZU/ZURAZU/SubCategory/ViewModel/SubCategoryViewModel.swift
@@ -10,7 +10,9 @@ import Combine
 
 protocol SubCategoryViewModelType {
   
-  var subCategories: PassthroughSubject<[SubCategory], Never> { get }
+  var subCategories: CurrentValueSubject<[SubCategory], Never> { get }
+  var categoryProducts: PassthroughSubject<[Product], Never> { get }
+  var selectedCategoryIndex: PassthroughSubject<IndexPath, Never> { get }
   var startFetching: PassthroughSubject<Void, Never> { get }
   var close: PassthroughSubject<Void, Never> { get }
 }
@@ -18,9 +20,13 @@ protocol SubCategoryViewModelType {
 final class SubCategoryViewModel: SubCategoryViewModelType {
   
   private let mainCategory: MainCategory
+  private var offset: Int = 0
+  private let limit: Int = 20
   private var cancellables: Set<AnyCancellable> = []
   
-  var subCategories: PassthroughSubject<[SubCategory], Never> = .init()
+  var subCategories: CurrentValueSubject<[SubCategory], Never> = .init([])
+  var categoryProducts: PassthroughSubject<[Product], Never> = .init()
+  var selectedCategoryIndex: PassthroughSubject<IndexPath, Never> = .init()
   var startFetching: PassthroughSubject<Void, Never> = .init()
   var close: PassthroughSubject<Void, Never> = .init()
   
@@ -42,6 +48,21 @@ final class SubCategoryViewModel: SubCategoryViewModelType {
         self?.closeView()
       }
       .store(in: &cancellables)
+    
+    selectedCategoryIndex
+      .removeDuplicates()
+      .map { index -> Int? in
+        if index == IndexPath.first {
+          return nil
+        }
+        let selectedCategory = self.subCategories.value[index.row]
+        
+        return selectedCategory.idx
+      }
+      .sink { [weak self] in
+        self?.fetchCategoryProducts(at: $0)
+      }
+      .store(in: &cancellables)
   }
 }
 
@@ -49,17 +70,43 @@ private extension SubCategoryViewModel {
   
   func fetchSubCategories() {
     let networkProvider: NetworkProvider = .init()
-    
-    let subCategoryPublisher: AnyPublisher<Result<BaseResponse<SubCategory>, NetworkError>, Never> = networkProvider.request(route: SubCategoryEndPoint.subCategories(mainIndex: mainCategory.idx))
+    let subCategoryPublisher: AnyPublisher<Result<BaseResponse<[SubCategory]>, NetworkError>, Never> = networkProvider.request(route: SubCategoryEndPoint.subCategories(mainIndex: mainCategory.idx))
     
     subCategoryPublisher
       .receive(on: Scheduler.main)
       .sink { [weak self] result in
         switch result {
         case .success(let responseResult):
-          guard let subCategories: [SubCategory] = responseResult.list else { return }
+          guard var subCategories: [SubCategory] = responseResult.list,
+                let mainCategory = self?.mainCategory
+          else { return }
+          let category = SubCategory(idx: 0, mainCategory: mainCategory, korean: "전체", english: "all", priority: 0)
+          subCategories.insert(category, at: 0)
           
           self?.subCategories.send(subCategories)
+          self?.offset = 0
+        case .failure(let error):
+          print(error.localizedDescription)
+        }
+      }
+      .store(in: &cancellables)
+  }
+  
+  // MARK: - 해당 관련 코드는 미완성이므로 수정이 필요합니다!
+  func fetchCategoryProducts(at subCategoryIdx: Int?) {
+    let networkProvider: NetworkProvider = .init()
+    
+    let categoryProductsPublisher: AnyPublisher<Result<BaseResponse<Products>, NetworkError>, Never> = networkProvider.request(route: SubCategoryEndPoint.categoryProducts(offset: offset, limit: limit, mainCategoryIdx: mainCategory.idx, subCategoryIdx: subCategoryIdx, notOnlySelectProgressing: false))
+    
+    categoryProductsPublisher
+      .sink { [weak self] result in
+        switch result {
+        case .success(let responseResult):
+          guard let data = responseResult.list else { return }
+          
+          self?.categoryProducts.send(data.products)
+          self?.nextOffset()
+          
         case .failure(let error):
           print(error.localizedDescription)
         }
@@ -69,5 +116,9 @@ private extension SubCategoryViewModel {
   
   func closeView() {
     SceneCoordinator.shared.close(animated: true)
+  }
+  
+  func nextOffset() {
+    offset += limit
   }
 }
