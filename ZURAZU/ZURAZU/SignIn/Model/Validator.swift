873825e0135e632ea91038c9c186ea7c080bbbe5
Seo Myung Ryul <smr3102@naver.com>
2021-06-03 21:14:34 +0900

    [FEAT] 리뷰 반영 https://github.com/zurazu/zurazu_iOS/pull/23#discussion_r644721233

--- ZURAZU/ZURAZU/SignIn/Model/Validator.swift
+++ ZURAZU/ZURAZU/SignIn/Model/Validator.swift
@@ -10,15 +10,15 @@ import Foundation
 struct Validator {
   
   static func isValid(email: String) -> Bool {
-    let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    let predicate = NSPredicate(format:"SELF MATCHES %@", regex)
+    let regex: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    let predicate: NSPredicate = .init(format:"SELF MATCHES %@", regex)
     
     return predicate.evaluate(with: email)
   }
   
   static func isValid(password: String) -> Bool {
-    let regex = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,16}"
-    let predicate = NSPredicate(format:"SELF MATCHES %@", regex)
+    let regex: String = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,16}"
+    let predicate: NSPredicate = .init(format:"SELF MATCHES %@", regex)
     
     return predicate.evaluate(with: password)
   }
