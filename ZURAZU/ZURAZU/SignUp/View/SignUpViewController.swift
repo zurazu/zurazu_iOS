50d70fe4e574db186d9ae49246fd23800b919eb3
Seo Myung Ryul <smr3102@naver.com>
2021-06-04 16:14:28 +0900

    [FEAT] SignUpViewController 생성

---
+++ ZURAZU/ZURAZU/SignUp/View/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/06/04.
+//
+
+import UIKit
+import Combine
+import CombineCocoa
+
+final class SignUpViewController: UIViewController, ViewModelBindableType {
+  
+  var viewModel: SignUpViewModelType?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  func bindViewModel() {
+    
+  }
+}
---
+++ ZURAZU/ZURAZU/SignUp/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/06/04.
+//
+
+import Foundation
