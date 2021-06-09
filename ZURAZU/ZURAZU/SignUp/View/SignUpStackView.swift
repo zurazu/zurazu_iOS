a385664e6d546b9460d13bc6f033a63b02682363
Seo Myung Ryul <smr3102@naver.com>
2021-06-09 19:32:36 +0900

    [FEAT] 파일 추가

---
+++ ZURAZU/ZURAZU/SignUp/View/SignUpStackView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpStackView.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/06/09.
+//
+
+import UIKit
+
+class SignUpStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ZURAZU/ZURAZU/SignUp/View/TitleView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleView.swift
+//  ZURAZU
+//
+//  Created by 서명렬 on 2021/06/09.
+//
+
+import UIKit
+
+class TitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
