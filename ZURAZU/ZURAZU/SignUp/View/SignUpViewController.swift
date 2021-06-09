95fbfc6dbde2f1cd3ca6be51139365efcfc026bd
Seo Myung Ryul <smr3102@naver.com>
2021-06-09 19:42:07 +0900

    [FEAT] SignUp화면 UI 및 바인딩 작성

--- ZURAZU/ZURAZU/SignUp/View/SignUpViewController.swift
+++ ZURAZU/ZURAZU/SignUp/View/SignUpViewController.swift
@@ -14,9 +14,28 @@ final class SignUpViewController: UIViewController, ViewModelBindableType {
   var viewModel: SignUpViewModelType?
   
   private lazy var backButton: UIButton = {
-    let button = UIButton(frame: .zero)
+    let button: UIButton = .init(frame: .zero)
     button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
     button.tintColor = .monoPrimary
+    
+    return button
+  }()
+  
+  let stackView: SignUpStackView = .init(frame: .zero)
+  private lazy var scrollView: UIScrollView = {
+    let scrollView: UIScrollView = .init(frame: .zero)
+    
+    scrollView.showsVerticalScrollIndicator = false
+    scrollView.showsHorizontalScrollIndicator = false
+    scrollView.contentInset = UIEdgeInsets(top: 36, left: 0, bottom: 36, right: 0)
+    
+    return scrollView
+  }()
+  
+  private lazy var signUpButton: SignButton = {
+    let button: SignButton = .init(frame: .zero)
+    
+    button.setTitle("회원가입", for: .normal)
     
     return button
   }()
@@ -32,13 +51,16 @@ final class SignUpViewController: UIViewController, ViewModelBindableType {
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    
     navigationController?.setNavigationBarHidden(false, animated: animated)
+    tabBarController?.tabBar.isHidden = true
   }
   
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     
     navigationController?.setNavigationBarHidden(true, animated: animated)
+    tabBarController?.tabBar.isHidden = false
   }
   
   func bindViewModel() {
@@ -46,6 +68,70 @@ final class SignUpViewController: UIViewController, ViewModelBindableType {
       .tapPublisher
       .sink { [weak self] in
         self?.viewModel?.closeEvent.send()
+      }
+      .store(in: &cancellables)
+    
+    signUpButton
+      .tapPublisher
+      .sink {
+        
+      }
+      .store(in: &cancellables)
+    
+    stackView.emailInputView.textField
+      .textPublisher
+      .compactMap { $0 }
+      .filter { !$0.isEmpty }
+      .sink { [weak self] in
+        self?.viewModel?.email.send($0)
+      }
+      .store(in: &cancellables)
+    
+    stackView.passwordInputView.textField
+      .textPublisher
+      .compactMap { $0 }
+      .sink { [weak self] in
+        self?.viewModel?.password.send($0)
+      }
+      .store(in: &cancellables)
+    
+    stackView.confirmPasswordInputView.textField
+      .textPublisher
+      .compactMap { $0 }
+      .sink { [weak self] in
+        self?.viewModel?.confirmPassword.send($0)
+      }
+      .store(in: &cancellables)
+    
+    viewModel?.isValid
+      .receive(on: Scheduler.main)
+      .removeDuplicates()
+      .sink {
+        self.signUpButton.isEnabled = $0
+      }
+      .store(in: &cancellables)
+    
+    viewModel?.isEmailValid
+      .receive(on: Scheduler.main)
+      .removeDuplicates()
+      .sink { [weak self] in
+        self?.stackView.emailInputView.showMessage(with: $0)
+      }
+      .store(in: &cancellables)
+    
+    viewModel?.isPasswordValid
+      .receive(on: Scheduler.main)
+      .removeDuplicates()
+      .sink { [weak self] in
+        self?.stackView.passwordInputView.showMessage(with: $0)
+      }
+      .store(in: &cancellables)
+    
+    viewModel?.isConfirmPasswordValid
+      .receive(on: Scheduler.main)
+      .removeDuplicates()
+      .sink { [weak self] in
+        self?.stackView.confirmPasswordInputView.showMessage(with: $0)
       }
       .store(in: &cancellables)
   }
@@ -60,6 +146,28 @@ private extension SignUpViewController {
   }
   
   func setupConstraint() {
-
+    [scrollView, signUpButton].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview($0)
+    }
+    
+    scrollView.addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      scrollView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+      
+      stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+      stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+      stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+      
+      signUpButton.topAnchor.constraint(equalTo: scrollView.bottomAnchor),
+      signUpButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15),
+      signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      signUpButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+      signUpButton.heightAnchor.constraint(equalToConstant: 53)
+    ])
   }
 }
--- ZURAZU/ZURAZU/SignUp/ViewModel/SignUpViewModel.swift
+++ ZURAZU/ZURAZU/SignUp/ViewModel/SignUpViewModel.swift
@@ -10,12 +10,30 @@ import Combine
 
 protocol SignUpViewModelType {
   
+  var email: CurrentValueSubject<String, Never> { get }
+  var password: CurrentValueSubject<String, Never> { get }
+  var confirmPassword: CurrentValueSubject<String, Never> { get }
+  var name: CurrentValueSubject<String, Never> { get }
   var closeEvent: PassthroughSubject<Void, Never> { get }
+  
+  var isValid: PassthroughSubject<Bool, Never> { get }
+  var isEmailValid: PassthroughSubject<Bool, Never> { get }
+  var isPasswordValid: PassthroughSubject<Bool, Never> { get }
+  var isConfirmPasswordValid: PassthroughSubject<Bool, Never> { get }
 }
 
 final class SignUpViewModel: SignUpViewModelType {
   
+  var email: CurrentValueSubject<String, Never> = .init("")
+  var password: CurrentValueSubject<String, Never> = .init("")
+  var confirmPassword: CurrentValueSubject<String, Never> = .init("")
+  var name: CurrentValueSubject<String, Never> = .init("")
   var closeEvent: PassthroughSubject<Void, Never> = .init()
+  
+  var isValid: PassthroughSubject<Bool, Never> = .init()
+  var isEmailValid: PassthroughSubject<Bool, Never> = .init()
+  var isPasswordValid: PassthroughSubject<Bool, Never> = .init()
+  var isConfirmPasswordValid: PassthroughSubject<Bool, Never> = .init()
   
   private var cancellables: Set<AnyCancellable> = []
   
@@ -30,6 +48,36 @@ private extension SignUpViewModel {
   func bind() {
     closeEvent
       .sink { SceneCoordinator.shared.close(animated: true) }
+      .store(in: &cancellables)
+    
+    email
+      .sink {
+        self.isEmailValid.send(Validator.isValid(email: $0))
+      }
+      .store(in: &cancellables)
+    
+    password
+      .sink {
+        self.isPasswordValid.send(Validator.isValid(password: $0))
+        
+        if !self.confirmPassword.value.isEmpty {
+          self.isConfirmPasswordValid.send(self.confirmPassword.value == $0)
+        }
+      }
+      .store(in: &cancellables)
+    
+    confirmPassword
+      .sink {
+        self.isConfirmPasswordValid.send(self.password.value == $0)
+      }
+      .store(in: &cancellables)
+    
+    isEmailValid.combineLatest(isPasswordValid, isConfirmPasswordValid)
+      .map { $0 && $1 && $2 }
+      .removeDuplicates()
+      .sink {
+        self.isValid.send($0)
+      }
       .store(in: &cancellables)
   }
 }
