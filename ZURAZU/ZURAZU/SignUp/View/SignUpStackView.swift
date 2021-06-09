d7dac4261d0cf9d76191d93f3aa44ee6b6b76ef8
Seo Myung Ryul <smr3102@naver.com>
2021-06-09 19:37:42 +0900

    [FEAT] SignUp화면을 위한 UIStackView 작성
    - InputView 관련 화면만 작성
    - PickerView와 개인정보 동의 관련 뷰는 추가적으로 작성예정

--- ZURAZU/ZURAZU/SignUp/View/SignUpStackView.swift
+++ ZURAZU/ZURAZU/SignUp/View/SignUpStackView.swift
@@ -7,14 +7,47 @@
 
 import UIKit
 
-class SignUpStackView: UIStackView {
+final class SignUpStackView: UIStackView {
+  
+  let emailInputView: InputView = .init(frame: .zero, inputViewType: .email)
+  let passwordInputView: InputView = .init(frame: .zero, inputViewType: .password)
+  let confirmPasswordInputView: InputView = .init(frame: .zero, inputViewType: .confirmPassword)
+  let nameInputView: InputView = .init(frame: .zero, inputViewType: .name)
+  let genderPickerView: UIPickerView = .init(frame: .zero)
+  let birthPickerView: UIDatePicker = .init(frame: .zero)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupView()
+  }
+  
+  required init(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+private extension SignUpStackView {
+  
+  func setupView() {
+    spacing = 25
+    axis = .vertical
+    alignment = .fill
+    
+    passwordInputView.textField.isSecureTextEntry = true
+    confirmPasswordInputView.textField.isSecureTextEntry = true
+    
+    addArrangedTitleView(emailInputView, title: "이메일 주소 (ID)", isNecessary: true)
+    addArrangedTitleView(passwordInputView, title: "비밀번호", isNecessary: true)
+    addArrangedTitleView(confirmPasswordInputView, title: "비밀번호 확인", isNecessary: true)
+    addArrangedTitleView(nameInputView, title: "실명")
+  }
+  
+  func addArrangedTitleView(_ view: UIView, title: String, isNecessary: Bool = false) {
+    let titleView: TitleView = .init(frame: .zero, contentView: view, isNecessary: isNecessary)
+    
+    titleView.updateTitle(with: title)
+    
+    addArrangedSubview(titleView)
+  }
 }
