c7915ee91d6e68779e43eb253d2a913af412eeed
Seo Myung Ryul <smr3102@naver.com>
2021-06-09 19:36:03 +0900

    [FEAT] SignUp화면에서 사용할 TitleView 작성
    - 내부에 contentView를 변경하며 재활용 가능
    - updateTitle(with:) 메서드를 통해 title 수정

--- ZURAZU/ZURAZU/SignUp/View/TitleView.swift
+++ ZURAZU/ZURAZU/SignUp/View/TitleView.swift
@@ -7,14 +7,70 @@
 
 import UIKit
 
-class TitleView: UIView {
+final class TitleView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+  lazy var titleLabel: UILabel = {
+    let label: UILabel = .init(frame: .zero)
+    
+    label.font = .primaryBold
+    label.textColor = .monoPrimary
+    
+    return label
+  }()
+  
+  lazy var necessaryLabel: UILabel = {
+    let label: UILabel = .init(frame: .zero)
+    
+    label.font = .quaternaryBold
+    label.textColor = .redPrimary
+    label.text = " *"
+    
+    return label
+  }()
+  
+  let contentView: UIView
+  
+  init(frame: CGRect, contentView: UIView, isNecessary: Bool = false) {
+    self.contentView = contentView
+    super.init(frame: frame)
+    
+    setupView()
+    setupConstraint()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func updateTitle(with title: String) {
+    titleLabel.text = title
+  }
+}
+
+private extension TitleView {
+  
+  func setupView() {
+
+  }
+  
+  func setupConstraint() {
+    [titleLabel, necessaryLabel, contentView].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      addSubview($0)
     }
-    */
-
+    
+    NSLayoutConstraint.activate([
+      titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+      titleLabel.topAnchor.constraint(equalTo: topAnchor),
+      titleLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 0),
+      
+      necessaryLabel.leadingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+      
+      contentView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 15),
+      contentView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+      contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+      contentView.heightAnchor.constraint(equalToConstant: 40),
+      contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+    ])
+  }
 }
