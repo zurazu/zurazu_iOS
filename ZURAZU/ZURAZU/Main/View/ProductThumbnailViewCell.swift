e0e78fa2235cf9dcea2c4546c80d1364f82d1a19
itzel.du <itzel.du@kakaoenterprise.com>
2021-06-19 23:38:10 +0900

    [FEAT] PICK label view 생성

--- ZURAZU/ZURAZU/Main/View/ProductThumbnailViewCell.swift
+++ ZURAZU/ZURAZU/Main/View/ProductThumbnailViewCell.swift
@@ -17,6 +17,12 @@ final class ProductThumbnailViewCell: UICollectionViewCell, Reusable {
   }()
   
   private let infoView: ProductThumbnailInfoView = .init(frame: .zero)
+  private let pickLabel: LabelView = {
+    let label: LabelView = .init(frame: .zero)
+    label.setup(title: "PICK")
+    
+    return label
+  }()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -31,12 +37,13 @@ final class ProductThumbnailViewCell: UICollectionViewCell, Reusable {
   func update(image: UIImage, info: ProductThumbnailInfo, size: ProductThumbnailSize) {
     imageView.image = image
     infoView.update(info: info, size: size)
+    pickLabel.isHidden = size != .medium
   }
   
   private func setupConstraint() {
     translatesAutoresizingMaskIntoConstraints = false
     
-    [imageView, infoView].forEach {
+    [imageView, infoView, pickLabel].forEach {
       $0.translatesAutoresizingMaskIntoConstraints = false
       addSubview($0)
     }
@@ -52,7 +59,53 @@ final class ProductThumbnailViewCell: UICollectionViewCell, Reusable {
       infoView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5),
       infoView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
       infoView.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -inset),
-      infoView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+      infoView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+      
+      pickLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 2),
+      pickLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -2)
+    ])
+  }
+  
+}
+
+final class LabelView: UIView {
+  
+  private let label: UILabel = .init()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    setupConstraint()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setup(title: String) {
+    label.attributedText = NSAttributedString.init(string: title, attributes: [
+      .foregroundColor : UIColor.white,
+      .font            : UIFont.quaternaryBold
+    ])
+    
+    backgroundColor = .bluePrimary
+  }
+  
+  private func setupConstraint() {
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    [label].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+      addSubview($0)
+    }
+    
+    let inset: CGFloat = 2
+    
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: topAnchor, constant: inset),
+      label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset),
+      label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset),
+      label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -inset)
     ])
   }
   
